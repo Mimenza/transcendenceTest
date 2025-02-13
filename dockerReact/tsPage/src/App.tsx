cda2fc9bd3b7bbc1f883dcc4ca9dc8bdaa403bbd
Mimenza <mimenzae@gmail.com>
2025-02-13 22:35:47 +0100

    login/signup navigation created

--- dockerReact/tsPage/src/App.tsx
+++ dockerReact/tsPage/src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import LoginScreen from './screens/loginScreen';
+import SignupScreen from './screens/signupScreen';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div className="dark:bg-gray-900 dark:text-white">
-      <LoginScreen />
+      <Router>
+        <Routes>
+          {/* Ruta por defecto redirige a /login */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/signup" element={<SignupScreen />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- dockerReact/tsPage/src/screens/signupScreen.tsx
+++ dockerReact/tsPage/src/screens/signupScreen.tsx
@@ -3,7 +3,7 @@ import {useState} from "react";
 import {Eye, EyeOff} from "lucide-react";
 import {Link} from "react-router-dom";
 
-const LoginScreen = () => {
+const SignupScreen = () => {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
@@ -31,7 +31,7 @@ const LoginScreen = () => {
                     </a>
                 </div>
                 <h1 className="text-xl font-bold text-gray-900 md:text-2xl dark:text-white text-center mb-4">
-                    Log in to your account
+                    Create an account
                 </h1>
                 <form className="space-y-4" action="#">
                     <div>
@@ -46,6 +46,20 @@ const LoginScreen = () => {
                             id="email"
                             className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="transcendence@chamanismosSL.com"
+                            required
+                        />
+                    </div>
+                    <div className="relative">
+                        <label
+                            htmlFor="username"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Username
+                        </label>
+                        <input
+                            id="username"
+                            placeholder="Generic username"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pr-10"
                             required
                         />
                     </div>
@@ -71,58 +85,19 @@ const LoginScreen = () => {
                             {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                         </button>
                     </div>
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input
-                                id="remember"
-                                type="checkbox"
-                                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                            />
-                            <label
-                                htmlFor="remember"
-                                className="ml-2 text-sm text-gray-500 dark:text-gray-300"
-                            >
-                                Remember me
-                            </label>
-                        </div>
-                        <a
-                            href="#"
-                            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                        >
-                            Forgot password?
-                        </a>
-                    </div>
                     <button
                         type="submit"
                         className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     >
-                        Log in
-                    </button>
-                    <div className="relative flex items-center justify-center w-full my-8">
-                        <hr className="w-64 h-px bg-gray-200 border-0 dark:bg-gray-700 my-6" />
-                        <span className="absolute px-3 font-medium text-gray-900 bg-white left-1/2 transform -translate-x-1/2 dark:text-white dark:bg-gray-800">
-                            or
-                        </span>
-                    </div>
-                    <button
-                        //type="submit"
-                        className="w-full text-white bg-rose-600 hover:bg-rose-700 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800"
-                    >
-                        Log in with Google
-                    </button>
-                    <button
-                        //type="submit"
-                        className="w-full text-white bg-secondary-600 hover:bg-secondary-700 focus:ring-4 focus:outline-none focus:ring-secondary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-secondary-600 dark:hover:bg-secondary-700 dark:focus:ring-secondary-800"
-                    >
-                        Log in with 42
+                        Sign up
                     </button>
                     <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
-                        Don’t have an account yet?{" "}
+                        Already have an account?{" "}
                         <Link
-                            to="/signup"
+                            to="/login"
                             className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                         >
-                            Sign in
+                            Log in
                         </Link>
                     </p>
                 </form>
@@ -131,4 +106,4 @@ const LoginScreen = () => {
     );
 };
 
-export default LoginScreen;
+export default SignupScreen;
